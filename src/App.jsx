96f96e73cc946497ce1a2c9153d0ed3446bf6c11
alejo-pioli <apioli@facultad.sanfrancisco.utn.edu.ar>
2025-06-05 19:58:23 -0300

    get alumnos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [alumnos, setAlumnos] = useState([])
+
+  function getAlumnos() {
+    axios.get("http://localhost:3000/api/alumnos")
+      .then((data) => {
+        console.log(data.data)
+        setAlumnos(data.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
+  useEffect(() => {
+    getAlumnos()
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Alumnos</h1>
+      {alumnos.length === 0 ? <></> :
+        alumnos.map((alumno) => {
+          return <li>{alumno.name} {alumno.surname}</li>
+        })}
     </>
   )
 }
