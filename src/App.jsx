28b039573cb245523d5a53713418085a36782357
alejo-pioli <apioli@facultad.sanfrancisco.utn.edu.ar>
2025-06-05 20:20:57 -0300

    query params

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Button } from 'react-bootstrap'
 
 function App() {
   const [alumnos, setAlumnos] = useState([])
 
-  function getAlumnos() {
-    axios.get("http://localhost:3000/api/alumnos")
+  function getAlumnos(materia) {
+    const url = "http://localhost:3000/api/alumnos"
+    let params
+    
+    if (materia) {
+      params = `?materia=${materia}`
+    } else {
+      params = ""
+    }
+
+    axios.get(url + params)
       .then((data) => {
         console.log(data.data)
         setAlumnos(data.data)
@@ -16,12 +26,16 @@ function App() {
   }
 
   useEffect(() => {
-    getAlumnos()
+    getAlumnos(null)
   }, [])
 
   return (
     <>
       <h1>Alumnos</h1>
+      <form>
+        <input type="text"></input>
+        <Button variant="primary">Buscar</Button>
+      </form>
       {alumnos.length === 0 ? <></> :
         alumnos.map((alumno) => {
           return <li>{alumno.name} {alumno.surname}</li>
